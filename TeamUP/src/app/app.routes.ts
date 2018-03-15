ed4c7b0a8cc6c9b9ab4bca29a75941e84912802f
AravindM-Accolite <aravindkumar.metku@github.com>
2018-03-15 21:13:14 +0530

    1. created service 2. initial commit for matches component 3. initial commit for dashboard component

--- TeamUP/src/app/app.routes.ts
+++ TeamUP/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import {UserinfoComponent} from "./userinfo/userinfo.component";
 import {PagenotfoundComponent} from "./pagenotfound/pagenotfound.component";
 import {UserBasicComponent} from "./userinfo/basic/userbasic.component";
 import {UserProfileComponent} from "./userinfo/user-profile/user-profile.component";
+import {DashboardComponent} from "./userinfo/dashboard/dashboard.component";
+import {MatchesComponent} from "./userinfo/matches/matches.component";
 
 export const AppRoutes: Routes = [
   {
@@ -16,7 +18,16 @@ export const AppRoutes: Routes = [
       {
         path: 'profile',
         component: UserProfileComponent
-      }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'matches',
+        component: MatchesComponent
+      },
+
     ]
   },
   { path: '**', component: PagenotfoundComponent }
